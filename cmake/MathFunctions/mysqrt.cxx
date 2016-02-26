b85b6e627b5646b491defa4427a163018fa356b1
Jayson Tung <jaysontung@gmail.com>
2016-02-26 13:29:51 -0500

    Add Step 3 lab 5

---
+++ cmake/MathFunctions/mysqrt.cxx
@@ -0,0 +1,22 @@
+const double ACCURACY=0.001;
+
+// Newton's Approximation Method
+double mysqrt(const double number) {
+	double lower, upper, guess;
+	if (number < 1) {
+		lower = number;
+		upper = 1;
+	}
+	else {
+		lower = 1;
+		upper = number;
+	}
+	while ((upper-lower) > ACCURACY) {
+		guess = (lower + upper)/2;
+		if (guess*guess > number)
+		upper =guess;
+		else {
+			lower = guess; 
+		}
+	return (lower + upper)/2;
+}
